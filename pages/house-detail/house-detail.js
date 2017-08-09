02184b632c57ab52b5279dfa6070e519650fa3f1
dada <zhangda733@gmail.com>
2017-08-09 23:56:27 +0800

    basic line chart work

--- pages/house-detail/house-detail.js
+++ pages/house-detail/house-detail.js
@@ -1,23 +1,77 @@
 // house-detail.js
 var util = require("../../utils/util.js")
-
+var wxCharts = require('../../utils/wxcharts.js');
+var app = getApp();
+var lineChart = null;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    chartData:[]
   },
+
+  touchHandler: function (e) {
+        console.log(lineChart.getCurrentDataIndex(e));
+        lineChart.showToolTip(e, {
+            // background: '#7cb5ec'
+        });
+    },    
+    updateData: function () {
+        var simulationData = this.createSimulationData();
+        var series = [{
+            name: '成交量1',
+            data: simulationData.data,
+            format: function (val, name) {
+                return val.toFixed(2) + '万';
+            }
+        }];
+        lineChart.updateData({
+            categories: simulationData.categories,
+            series: series
+        });
+    },
 
   fetchTrend: function(url) {
     var page = this;
     var houseList = []
     console.log("Request trend url " + url)
+    var tmpChart = lineChart;
+    console.log("chart : " +lineChart);
     util.getData(url)
       .then(function (res) {
         console.log("Status:" + res.statusCode);
         console.log("Get Trend :" + res.data.data);
+
+        var house = JSON.parse(res.data.data);
+        house = house[0];
+        var title = house["title"];
+        var trendList = house["trendList"];
+        console.log("trends:" + trendList);
+        var trendsPrice = []
+        var trendsDate = []
+        for(var index in trendList) {
+            var trend = trendList[index];
+            console.log("Trend : " + trend);
+            trendsPrice.push(trend.price);
+            trendsDate.push(trend.date);
+        }
+        // page.setData({chartData : trends});
+        var series = [{
+            name: '挂牌价',
+            data: trendsPrice,
+            format: function (val, name) {
+                return val.toFixed(2) + '万';
+            }
+        }];
+        console.log("chart 2 : " + lineChart);
+        console.log("prices : " + trendsPrice);
+        console.log("dates : " + trendsDate);
+        tmpChart.updateData({
+            categories: trendsDate,
+            series: series
+        });
       });  
   },
 
@@ -34,7 +88,9 @@ Page({
     //for debug
     this.setData({houseid : options.houseid})
     // this.setData({ estateName: "金地未未来" });
+    this.loadChart();
     this.load();
+
   },
 
   /**
@@ -84,5 +140,52 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+
+  loadChart: function() {
+    var windowWidth = 320;
+        try {
+            var res = wx.getSystemInfoSync();
+            windowWidth = res.windowWidth;
+        } catch (e) {
+            console.error('getSystemInfoSync failed!');
+        }
+        
+        var page = this;
+        lineChart = new wxCharts({
+            canvasId: 'lineCanvas',
+            type: 'line',
+            categories: [1],
+            animation: true,
+            background: '#f5f5f5',
+            title: "历史趋势",
+            series: [{
+                name: '挂牌价',
+                data: [1],
+                format: function (val, name) {
+                    return val.toFixed(2) + '万';
+                }
+            }],
+            xAxis: {
+                title: '时间',
+                disableGrid: true
+            },
+            yAxis: {
+                title: '价格 (万元)',
+                format: function (val) {
+                    return val.toFixed(2);
+                },
+                min: 0
+            },
+            width: windowWidth,
+            height: 200,
+            dataLabel: false,
+            dataPointShape: true,
+            extra: {
+                lineStyle: 'curve'
+            }
+        });
+        console.log("After set chart " + lineChart);
+  },
+
 })
